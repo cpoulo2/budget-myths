86a03d253f5c83cf519c2e834eceeff488699c77
cpoulo2 <cdpoulos@gmail.com>
2025-08-19 14:50:21 -0500

    copy edits

--- app.py
+++ app.py
@@ -369,7 +369,7 @@ We can break this down to understand which industries are capturing this value. 
         st.header("What If We Taxed Billionaire Wealth Like We Tax Working Class Wealth?")
 
         st.markdown("""
-The property tax is one of the most hated taxes and it's the poster child of the anti-tax movement. But it's worth considering why and what the property tax is. 
+The property tax is one of the most hated taxes. For this reason, it's used as the poster child of the anti-tax movement. But it's worth considering why and what the property tax is. 
         
 The property tax is a wealth tax, but a very <b><mark style='background-color: yellow'>narrow-based</mark></b> wealth tax. That means <b><mark style='background-color: yellow'>it applies only to a very *narrow* subsection of a larger potential tax base</mark></b>—in this case, one type of nonfinancial asset, land and the built structure ontop of it (i.e. your home), versus a broader base of financial and non-financial assets. 
         
